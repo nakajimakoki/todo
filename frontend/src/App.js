28563430cb61219d921570ce36de73bf06444968
Koki Nakajima <koki_nakajima@skynext.co.jp>
2025-07-13 11:04:27 +0900

    App.js: APIから取得して一覧表示する処理を追加

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+// frontend/src/App.js
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/todos')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('データ取得に失敗しました!!!!!!);
+        }
+        return response.json();
+      })
+      .then(data => setTodos(data))
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Todo一覧</h1>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            {todo.title} {todo.completed ? '✅' : '❌'}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
